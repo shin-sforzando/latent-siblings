ddd7a4c8938a2ed014642165b1213d623da91e74
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2019-07-25 17:42:37 +0900

    :+1: Add interpolation option to change method of resize

--- face_extractor.py
+++ face_extractor.py
@@ -3,8 +3,6 @@ from pathlib import Path
 import click
 import cv2
 
-HAAR_XML = 'venv/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml'
-
 '''
 OpenCV HAAR XML Default Options
 
@@ -14,6 +12,9 @@ OpenCV HAAR XML Default Options
 * venv/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_alt_tree.xml
 * venv/lib/python3.7/site-packages/cv2/data/haarcascade_profileface.xml
 '''
+
+HAAR_XML = 'venv/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml'
+INTERPOLATIONS = ['INTER_LINEAR', 'INTER_AREA', 'INTER_NEAREST', 'INTER_CUBIC', 'INTER_LANCZOS4']
 
 
 def print_version(ctx, param, value):
@@ -26,11 +27,14 @@ def print_version(ctx, param, value):
 @click.command()
 @click.argument('input_path', type=click.Path(exists=True))
 @click.option('-r', '--resolution', type=int, default=64, show_default=True, help='Resolution to Export')
+@click.option('-i', '--interpolation', type=click.Choice(INTERPOLATIONS), default=INTERPOLATIONS[4], show_default=True,
+              help='Method of Interpolation')
 @click.option('--version', is_flag=True, callback=print_version, expose_value=False, is_eager=True)
-def crop(input_path, resolution):
+def crop(input_path, resolution, interpolation):
     click.secho(f'Input: {input_path}', fg='green')
     resolution = (resolution, resolution)
     click.secho(f'Target Resolution: {resolution} [px]', fg='green')
+    click.secho(f'Interpolation Method: {interpolation}', fg='green')
     cascade = cv2.CascadeClassifier(HAAR_XML)
     image_color = cv2.imread(input_path)
     image_grey = cv2.imread(input_path, 0)
@@ -41,7 +45,7 @@ def crop(input_path, resolution):
         click.secho(f'Captured Face => X: {x} Y: {y} W: {w} H: {h}', fg='blue')
         output_path = Path(input_path).parent / f'cropped_{idx:03}.jpg'
         image_cropped = image_color[y:y + h, x:x + w]
-        image_resized = cv2.resize(image_cropped, resolution, interpolation=cv2.INTER_LANCZOS4)
+        image_resized = cv2.resize(image_cropped, resolution, interpolation=getattr(cv2, interpolation))
         cv2.imwrite(str(output_path), image_resized)
         click.secho(f'Output: {output_path}', fg='green')
 
